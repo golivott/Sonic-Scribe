992a3a9cbd4bad7f6d1631d438d862bbdab5ce94
Gabriel Olivotto <golivott@uwo.ca>
2023-11-21 15:20:44 -0500

    First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+    pathPrefix: "/Auto-Music-Transcriber",
+    siteMetadata: {
+        title: `Sonic Scribe`,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: ["gatsby-plugin-postcss"],
+};
